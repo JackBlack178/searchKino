23090650df7d8abe2f93218fd09884957943b72d
Ilyas Safiullin <Safiullin1Ilyas@yandex.ru>
2024-09-06 16:35:09 +0300

    Added alliases and setup tsconfig files to not show error with correct import pathes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
   },
 });
